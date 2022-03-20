c9c59f14b74cba78f484114150bc1ec28643d3c1
Ryong1998 <yoy0318@naver.com>
2022-03-20 17:25:34 +0900

    tabulate을 이용해서 데이텊레임 출력 연습

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
-print("hello")
+import pandas as pd
+from tabulate import tabulate
+
+df = pd.DataFrame({
+    "컬럼명1": [1, 2, 3],
+    "컬럼명2": [4, 5, 6],
+    "컬럼명3": [7, 8, 9]},
+
+     index=[2019, 2020, 2021]
+
+)
+df.index.name = '연도'
+print(df)
+print(tabulate(df, headers='keys', tablefmt='psql'))
--- show_covid_data.py
+++ show_covid_data.py
@@ -3,3 +3,78 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import datetime
+
+dt_now = datetime.datetime.now()
+enddate= "".join(str(dt_now.date()).split('-'))
+#print(enddate)
+startdate="20200120"
+
+service_key = 'OtlfaJxmWNmyxJI3MPVTRTrJKBOe7Qks7%2FTcHRZscnpf2e%2B4oKSbfzCMhU4RewP3729MnEvUD0pJgzJ8DIh3VQ%3D%3D' #키입력
+params = "&numOfRows=10&pageNo=1&startCreateDt="+startdate+"&endCreateDt="+enddate+"'" #요청변수 입력
+open_api = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?ServiceKey=' + service_key + params #사이트 키입력 입력변수 조합
+
+res = requests.get(open_api) #공공데이터는 네이버와 달리 headers 사용안하고 붙여서 쓰게 해놓는다
+
+
+#xml 파일 형식이므로 파싱과정 필요
+soup = BeautifulSoup(res.content, 'html.parser')
+
+# 공공api를 이용해서 받아올 정보들을 담을 리스트
+stateDt_list = list() # 기준날짜 리스트
+decideCnt_list = list() # 누적 확진자수 리스트
+accExamCnt_list = list() # 누적 검사자수 리스트
+deathCnt_list = list() # 누적 사망자수 리스트
+daily_decideCnt_list = list() # 일일 확진자수 리스트
+daily_accExamCnt_list = list() # 일일 검사자 수 리스트
+daily_deathCnt_list = list() # 일일 사망자수 리스트
+
+#원하는 정보의 (상위)태그들을 선택
+items = soup.select('item') 
+
+#  각 (상위)태그 들을 반복해서 안에 있는 하위 태그들의 정보들을 받아옴
+for item in items:
+    stateDt=item.select_one('stateDt') # 기준일을 태그로 받음
+    stateDt_list.append(stateDt.get_text())
+    decideCnt=item.select_one('decideCnt') # 누적확진자 수를 태그로 받음
+    decideCnt_list.append(int(decideCnt.get_text()))
+    accExamCnt=item.select_one('accExamCnt') # 누적 검사자 수를 태그로 받음
+    accExamCnt_list.append(int(accExamCnt.get_text()))
+    deathCnt=item.select_one('deathCnt') # 누적사망자 수를 태그로 받음
+    deathCnt_list.append(int(deathCnt.get_text()))
+    
+# 일일 확진자수,검사자수,사망자수 계산
+for data in range(len(stateDt_list)-1):
+    daily_decideCnt_list.append(decideCnt_list[data]-decideCnt_list[data+1]) # 일일 확진자수 리스트 생성
+    daily_accExamCnt_list.append(accExamCnt_list[data]-accExamCnt_list[data+1]) #일일 검사자수 리스트 생성
+    daily_deathCnt_list.append(deathCnt_list[data]-deathCnt_list[data+1]) # 일일 사망자수 리스트 생성
+daily_decideCnt_list.append(decideCnt_list[len(stateDt_list)-1]) # 처음의 일일 확진자수 리스트에 추가
+daily_accExamCnt_list.append(accExamCnt_list[len(stateDt_list)-1]) # 처음의 일일 검사자수 리스트에 추가
+daily_deathCnt_list.append(deathCnt_list[len(stateDt_list)-1]) # 처음의 일일 사망자수 리스트에 추가
+
+# 정보들을 담을 리스트를 확인하기 위한 출력
+# print(stateDt_list)
+# print(decideCnt_list)
+# print(accExamCnt_list)
+# print(deathCnt_list)
+
+
+
+#누적 지표, 일일 지표들 관련 데이터 프레임 생성 한 후 정보들을 담은 리스트를 삽입
+df = pd.DataFrame({
+    "기준날짜" : stateDt_list,
+    "누적확진자수" : decideCnt_list,
+    "누적검사자수" : accExamCnt_list,
+    "누적사망자수" : deathCnt_list,
+    "일일확진자수" : daily_decideCnt_list,
+    "일일검사자수" : daily_accExamCnt_list,
+    "일일사망자수" : daily_deathCnt_list},
+    
+)
+# 데이터 프레임의 인덱스를 설정
+df=df.set_index("기준날짜")
+
+# 인덱스를 날짜 순으로 정렬
+df=df.sort_index(axis = 0)
+
+print(df)
