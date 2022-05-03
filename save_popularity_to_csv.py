21e48c152bede87c0f4baa872c5b83ff9f28b798
Ryong1998 <yoy0318@naver.com>
2022-05-03 22:42:58 +0900

    드롭다운 클릭위해서 추후 firefox 다운 필요

--- save_popularity_to_csv.py
+++ save_popularity_to_csv.py
@@ -4,15 +4,26 @@ warnings.filterwarnings('ignore')
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select # 드롭다운 선택을 위햇 ㅓ임포트
 import time
 
 # 드라이버 생성
 # chromedriver 설치된 경로를 정확히 기재해야 함
 chromedriver = 'C:/Users/LG/dev_python/Webdriver/chromedriver.exe' # 윈도우 
-#chromedriver = '/usr/local/Cellar/chromedriver/chromedriver' # 맥
-driver = webdriver.Chrome(chromedriver)
+driver_chrome = webdriver.Chrome(chromedriver)
+
+driver_firefox=webdriver.Firefox() # Firefox 다운받아야 할듯
 
 # 크롤링할 사이트 호출
-driver.get("https://jumin.mois.go.kr/ageStatMonth.do")
+driver_chrome.get("https://jumin.mois.go.kr/ageStatMonth.do")
 time.sleep(2)
-driver.quit()
+
+elem = driver_chrome.find_element_by_id('sltOrgLvl1')
+elem.send_keys(Keys.RETURN)
+time.sleep(2)
+
+
+
+
+# driver_firefox.quit()
+driver_chrome.quit()
