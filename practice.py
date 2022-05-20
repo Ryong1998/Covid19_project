c5e351f23de63bf1e47c8f590b17795e479db7ed
Ryong1998 <yoy0318@naver.com>
2022-05-20 20:52:09 +0900

    도,시,군,구 등만 남게 전처리 하는 코드 구현

--- practice.py
+++ practice.py
@@ -1,38 +1,8 @@
-# 연도 및 달별 인구 구조 저장을 위한 코드
-import warnings
-warnings.filterwarnings('ignore')
+columns_list= ["경기도 군포시 (4141000000)", "경기도 군포시 군포1동(4141051000)","서울특별시  (1100000000)"]
+for i,column_value in enumerate(columns_list):
+    columns_list[i] = column_value.split('(')[0]
+    if columns_list[i][-1]==' ':
+        columns_list[i]=columns_list[i].split()
+        columns_list[i] = " ".join(columns_list[i])
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select # 드롭다운 선택을 위햇 ㅓ임포트
-import time
-
-# 드라이버 생성
-# chromedriver 설치된 경로를 정확히 기재해야 함
-chromeOptions = webdriver.ChromeOptions() # 크롬 드라이버 옵션 설정
-chromeOptions.add_argument("--profile-directory=AutoUser")
-prefs = {"download.default_directory" : "C:\self_project\covid_project\covid19_project\dataset"} # 파일다운로드 경로 설정
-chromeOptions.add_experimental_option("prefs",prefs) # 옵션 정의
-
-chromedriver = 'C:/Users/LG/dev_python/Webdriver/chromedriver.exe' # 윈도우 
-driver_chrome = webdriver.Chrome(executable_path=chromedriver, options=chromeOptions) # 설정 반영
-
-# 크롤링할 사이트 호출
-driver_chrome.get("https://jumin.mois.go.kr/ageStatMonth.do") # 셀리니움으로 크롤링할 메인 페이지 이동
-time.sleep(1)
-
-
-
-# csv 다운로드 까지 됨 - 근데 다운로드 하고 중지가 됨, 크롬 다운로드 상태바 안나오게 하는 설정 필요!
-download_btn = driver_chrome.find_element_by_id('csvDown') # 검색 태그 선택
-download_btn.click() # 태그 클릭
-time.sleep(2)
-driver_chrome.switch_to.alert.accept() #엔터키입력을 통해 다운로드 진행
-time.sleep(5)
-
-district_city = driver_chrome.find_element_by_name("sltOrgLvl1") #행정구역 시 태그 선택
-district_city_dropdown = Select(district_city) # 드롭다운 선택을 위해서 Select 함수 이용
-district_city_dropdown.select_by_index('1') #행정구역 시 상세선택
-time.sleep(1)
-
-driver_chrome.close()
+print(columns_list)
--- region_popularity_data_preprocessing.py
+++ region_popularity_data_preprocessing.py
@@ -6,18 +6,42 @@ from tabulate import tabulate # 데이터프레임을 예쁘게 출력하기 위
 
 # csv 파일 읽기, 경로는 상대경로, 절대경로 중 선택해서 기입
 
-df = pd.read_csv("C://self_project//covid_project//covid19_project//dataset//region_popularity//202112_202112_연령별인구현황_월간.csv", encoding='cp949')
+df = pd.read_csv("C://self_project//covid_project//covid19_project//dataset//region_popularity//202112_202112_연령별인구현황_월간 (100).csv", encoding='cp949')
 df = df.set_index('행정구역') # 데이터프레임의 인덱스 설정
 
 
 df = df.transpose() # 데이터 프레임의 컬럼들과 인덱스 설정(기존에는 인덱스가 지역, 컬럼이 연령별 카테고리 였는데 인덱스를 연령별 카테고리로, 컬럼을 지역으로 설정)
 season=df.index[0].split("_")[0] # 0000년00월 을 변수에 담음
+region_dosi = df.columns[0].split()[0]
 
-print(tabulate(df, headers='keys', tablefmt='psql'))
-print("index!!!!")
-print(df.index)
-print("columns!!!!")
-print(df.columns)
+region_sigungu = '_'+df.columns[0].split()[1]
+if '(' in region_sigungu:
+    region_sigungu = "_"
+
+columns_list = list(df.columns)
+print(columns_list)
+
+for i,column_value in enumerate(columns_list):
+    columns_list[i] = column_value.split('(')[0]
+    if columns_list[i][-1]==' ':
+        columns_list[i]=columns_list[i].split()
+        columns_list[i] = " ".join(columns_list[i])
+    
 print()
 print()
-print(season)
+print(columns_list)
+
+# # 만든 데이터 프레임 출력
+# print(tabulate(df, headers='keys', tablefmt='psql'))
+
+# print("index!!!!")
+# print(df.index) # 데이터 프레임의 인덱스 확인
+# print("columns!!!!")
+# print(df.columns) # 데이터 프레임의 컬럼들 확인
+# print()
+# print()
+
+filename = region_dosi+region_sigungu+season
+print(filename) # 만들고자 하는 파일명 임시적 확인
+
+# df.to_csv('C://self_project//covid_project//covid19_project//dataset//region_popularity_ver1//'+filename+'.csv')
