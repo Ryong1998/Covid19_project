74f4c28854df00af37cfc2d16f7fa26777bd8dfc
Ryong1998 <yoy0318@naver.com>
2022-05-30 19:27:55 +0900

    연도, 월, 성별, 나이컬럼 추가 부분 구현

--- region_popularity_data_preprocessing2.py
+++ region_popularity_data_preprocessing2.py
@@ -5,18 +5,29 @@ import pandas as pd
 from tabulate import tabulate # 데이터프레임을 예쁘게 출력하기 위해서 사용
 import os
 
+
+def func(df_data):
+    df_data = df_data.split('_')[0]   
+    return df_data
+
+
 # path_dir = 'C://self_project//covid_project//covid19_project//dataset//region_popularity'
 # file_lists = os.listdir(path_dir)
 
+# 임시 테스트를 위해서 파이 하나 만을 읽음
 df = pd.read_csv("C://self_project//covid_project//covid19_project//dataset//region_popularity_ver2//강원도.csv",  encoding='utf-8') # 추후 수정 필요
 
-for i in range(len(df['카테고리'])):
-    if df['카테고리'][i].split("_")[1] =="남" and df['카테고리'][i].split("_")[2] =="10~19세":
-        print(i)
-        print(df['카테고리'][i])
-        print(df.iloc[i])
-        print()
-        
+# 년도와 월을 컬럼으로 추가
+df['년월'] = df['카테고리'].str.split('_',expand=True)[0]
+df['년도'] = df['년월'].str.split('년',expand=True)[0]
+df['월'] = df['년월'].str.split('년',expand=True)[1].str.split('월',expand=True)[0]
 
-# print(df[df['카테고리']=='2021년12월_여_60~69세'])
+# 성별을 컬럼을 추가
+df['성별'] = df['카테고리'].str.split('_',expand=True)[1]
 
+# 나이를 컬럼으로 추가
+df['나이'] = df['카테고리'].str.split('_',expand=True)[2]
+
+print(df.info())
+
+df.head()
